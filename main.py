1e9a53dc776be0e29220789cf452bcdc9ba6e989
ASzwarc <szwarc.adam@gmail.com>
2019-09-15 15:31:01 +0200

    Extracted Board class to separate module in solver/board.

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ from __future__ import annotations
 import numpy as np
 from random import shuffle, choices, random
 import time
-from collections import namedtuple
+from solver.board import Board
 # TODO Maybe move algorithm parameters to separate config file
 POPULATION_SIZE = 20
 ELITISM_COEFF = 0.1
 DROP_OUT_COEFF = 0.5
 MUTATION_PROBABILITY = 0.1
 CROSSOVER_PROBABILITY = (1.0 - MUTATION_PROBABILITY) / 2.0
-
-Item = namedtuple("Item", ["row", "col", "value"])
 
 
 class Generation():
@@ -200,70 +198,6 @@ class Generation():
                                    Generation.DEFAULT_FITNESS])
         self._population = new_population.copy()
         return fittest_chromosome
-
-
-class Board():
-    def __init__(self):
-        self._items = []
-
-    @property
-    def items(self):
-        return self._items
-
-    def inject_board(self):
-        """
-        Helper function for injecting solvable sudoku board.
-        Board taken from this page, section "Easiest":
-        https://dingo.sbs.arizona.edu/~sandiway/sudoku/examples.html
-        """
-
-        board = []
-        board.append(Item._make([0, 3, 2]))
-        board.append(Item._make([0, 4, 6]))
-        board.append(Item._make([0, 6, 7]))
-        board.append(Item._make([0, 8, 1]))
-
-        board.append(Item._make([1, 0, 6]))
-        board.append(Item._make([1, 1, 8]))
-        board.append(Item._make([1, 4, 7]))
-        board.append(Item._make([1, 7, 9]))
-
-        board.append(Item._make([2, 0, 1]))
-        board.append(Item._make([2, 1, 9]))
-        board.append(Item._make([2, 5, 4]))
-        board.append(Item._make([2, 6, 5]))
-
-        board.append(Item._make([3, 0, 8]))
-        board.append(Item._make([3, 1, 2]))
-        board.append(Item._make([3, 3, 1]))
-        board.append(Item._make([3, 7, 4]))
-
-        board.append(Item._make([4, 2, 4]))
-        board.append(Item._make([4, 3, 6]))
-        board.append(Item._make([4, 5, 2]))
-        board.append(Item._make([4, 6, 9]))
-
-        board.append(Item._make([5, 1, 5]))
-        board.append(Item._make([5, 5, 3]))
-        board.append(Item._make([5, 7, 2]))
-        board.append(Item._make([5, 8, 8]))
-
-        board.append(Item._make([6, 2, 9]))
-        board.append(Item._make([6, 3, 3]))
-        board.append(Item._make([6, 7, 7]))
-        board.append(Item._make([6, 8, 4]))
-
-        board.append(Item._make([7, 1, 4]))
-        board.append(Item._make([7, 4, 5]))
-        board.append(Item._make([7, 7, 3]))
-        board.append(Item._make([7, 8, 6]))
-
-        board.append(Item._make([8, 0, 7]))
-        board.append(Item._make([8, 2, 3]))
-        board.append(Item._make([8, 4, 1]))
-        board.append(Item._make([8, 5, 8]))
-
-        self._items = board.copy()
 
 
 class GeneticSolution():
---
+++ solver/board.py
@@ -0,0 +1,67 @@
+from collections import namedtuple
+
+Item = namedtuple("Item", ["row", "col", "value"])
+
+
+class Board():
+    def __init__(self):
+        self._items = []
+
+    @property
+    def items(self):
+        return self._items
+
+    def inject_board(self):
+        """
+        Helper function for injecting solvable sudoku board.
+        Board taken from this page, section "Easiest":
+        https://dingo.sbs.arizona.edu/~sandiway/sudoku/examples.html
+        """
+
+        board = []
+        board.append(Item._make([0, 3, 2]))
+        board.append(Item._make([0, 4, 6]))
+        board.append(Item._make([0, 6, 7]))
+        board.append(Item._make([0, 8, 1]))
+
+        board.append(Item._make([1, 0, 6]))
+        board.append(Item._make([1, 1, 8]))
+        board.append(Item._make([1, 4, 7]))
+        board.append(Item._make([1, 7, 9]))
+
+        board.append(Item._make([2, 0, 1]))
+        board.append(Item._make([2, 1, 9]))
+        board.append(Item._make([2, 5, 4]))
+        board.append(Item._make([2, 6, 5]))
+
+        board.append(Item._make([3, 0, 8]))
+        board.append(Item._make([3, 1, 2]))
+        board.append(Item._make([3, 3, 1]))
+        board.append(Item._make([3, 7, 4]))
+
+        board.append(Item._make([4, 2, 4]))
+        board.append(Item._make([4, 3, 6]))
+        board.append(Item._make([4, 5, 2]))
+        board.append(Item._make([4, 6, 9]))
+
+        board.append(Item._make([5, 1, 5]))
+        board.append(Item._make([5, 5, 3]))
+        board.append(Item._make([5, 7, 2]))
+        board.append(Item._make([5, 8, 8]))
+
+        board.append(Item._make([6, 2, 9]))
+        board.append(Item._make([6, 3, 3]))
+        board.append(Item._make([6, 7, 7]))
+        board.append(Item._make([6, 8, 4]))
+
+        board.append(Item._make([7, 1, 4]))
+        board.append(Item._make([7, 4, 5]))
+        board.append(Item._make([7, 7, 3]))
+        board.append(Item._make([7, 8, 6]))
+
+        board.append(Item._make([8, 0, 7]))
+        board.append(Item._make([8, 2, 3]))
+        board.append(Item._make([8, 4, 1]))
+        board.append(Item._make([8, 5, 8]))
+
+        self._items = board.copy()
